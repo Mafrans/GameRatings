b89f899bcec140469130f8562f1d3215fb2c4047
Mafrans <malte.kluft@gmail.com>
2023-05-12 16:19:34 +0200

    fix: disable logging in production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,21 @@ import router from "./routes/router";
 import dotenv from "dotenv";
 
 dotenv.config();
-const server = fastify({ logger: process.env.NODE_ENV === "development" });
+const server = fastify({ logger: process.env.NODE_ENV !== "production" });
 
 // Register plugins
 server.register(cors);
 server.register(router);
 
-server.listen({
-  port: 3000,
-});
+server.listen(
+  {
+    port: 3000,
+  },
+  (err, addr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Started on ${addr}`);
+  }
+);
