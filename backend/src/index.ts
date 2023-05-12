3fcb4260bf1f9ce439e2532aeedc2ac0ce8ad3f7
Mafrans <malte.kluft@gmail.com>
2023-05-12 16:14:40 +0200

    feat: configure logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import { fastify } from "fastify";
+import cors from "@fastify/cors";
 import router from "./routes/router";
+import dotenv from "dotenv";
 
-const server = fastify({ logger: true });
+dotenv.config();
+const server = fastify({ logger: process.env.NODE_ENV === "development" });
 
+// Register plugins
+server.register(cors);
 server.register(router);
 
 server.listen({
