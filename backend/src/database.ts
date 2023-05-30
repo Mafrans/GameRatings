2acf5320b0d9f1c44bff55654972d92f1d4b1832
Mafrans <malte.kluft@gmail.com>
2023-05-30 17:30:37 +0200

    fix: disable verbose mode

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,4 @@
 import Database from "better-sqlite3";
 import { CustomDatabase } from "./types/CustomDatabase";
 
-export const db = new Database("database.sql", {
-  verbose: console.log,
-}) as CustomDatabase;
+export const db = new Database("database.sql") as CustomDatabase;
